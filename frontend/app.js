a95cd868259916ab20c36c0b59c65a2b83b1ca14
Sing Painter <mail@erlan.xyz>
2018-06-09 07:51:02 +0300

    + add method get_code + add method get_table_rows

--- frontend/app.js
+++ frontend/app.js
@@ -7,3 +7,24 @@ var eos = Eos(options);
 eos.getInfo((err, info) => {
 	console.log(err, info);
 });
+
+eos.getCode({ account_name: 'beesocial' }, (err, res) => {
+	if ( ! err) {
+		console.log(res.abi);
+	}
+	else console.error(err);
+});
+
+eos.getTableRows({
+	scope: 'beesocial',
+	code: 'beesocial',
+	table: 'skill',
+	json: true
+}, (err, res) => {
+	if ( ! err) {
+		res.rows.forEach(item => {
+			console.log('item', item);
+		});
+	}
+	else console.error(err);
+});
