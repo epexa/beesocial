c2b4a7760fa8a535ef7f1e7864ca9112621411e8
Sing Painter <mail@erlan.xyz>
2018-06-10 04:49:45 +0300

    * fix add resource

--- frontend/app.js
+++ frontend/app.js
@@ -1,17 +1,17 @@
 let options = {
-	//httpEndpoint: 'http://192.168.43.240:8888',
-	httpEndpoint: 'http://91.201.41.253:8888',
-	debug: true,
-	keyProvider: '5JBx24Y4w7BvA35JnScvhMYCvRjWoheKKQ6dt1R12ZV4PqB55Gh',
+	httpEndpoint: 'http://192.168.43.240:8888',
+	//httpEndpoint: 'http://91.201.41.253:8888',
+	debug: false,
+	keyProvider: '5JpNgFavmjG1Jp6m2hjpMFXF4Mhzpe8Bt2TzBy1dZjH3gFbE5jK',
 	//authorization: 'alice@active',
 	sign: true,
 	//broadcast: true
 };
 var eos = Eos(options);
 
-eos.getInfo((err, info) => {
+/* eos.getInfo((err, info) => {
 	console.log(err, info);
-});
+}); */
 
 eos.getCode({ account_name: 'beesocial' }, (err, res) => {
 	if ( ! err) {
@@ -34,8 +34,39 @@ let $resources = $resourcesPage.querySelector('#resources');
 let $resourceItem = document.querySelector('#resource-item'); */
 
 let getResources = () => {
-	
+	eos.getTableRows({
+		scope: 'beesocial',
+		code: 'beesocial',
+		table: 'resources',
+		json: true
+	}, (err, res) => {
+		if ( ! err) {
+			console.log(res);
+			res.rows.forEach(item => {
+				console.log('resource', item);
+				
+				/* let $newItem = $resourceItem.cloneNode(true);
+				$newItem.querySelector('.card-title').innerHTML = item.title;
+				$newItem.querySelector('.card-text').innerHTML = item.description;
+				$newItem.setAttribute('data-id', item.id);
+				$newItem.querySelector('button').addEventListener('click', () => {
+					resourceItemSelected = item;
+					$resourceModal.querySelector('#resource-title').innerHTML = item.title;
+					$resourceModal.querySelector('#resource-description').innerHTML = item.description;
+					$resourceModal.querySelector('#resource-how-get').innerHTML = item.howget;
+					$resourceModal.querySelector('#resource-contacts').innerHTML = item.contacts;
+					$resourceModal.querySelector('#resource-price').innerHTML = item.price;
+					resourceModal.show();
+				});
+				$newItem.style.display = 'block';
+				$resources.appendChild($newItem); */
+				
+			});
+		}
+		else console.error(err);
+	});
 };
+getResources();
 
 let getWorkersTable = () => {
 	eos.getTableRows({
@@ -75,13 +106,14 @@ let $login = document.getElementById('login');
 let $logout = document.getElementById('logout');
 
 let callbackAuth;
+let username;
 
 document.getElementById('form-login-pass').addEventListener('submit', e => {
 	e.preventDefault();
 	let log = document.getElementById('logged').checked,
 		user = document.getElementById('input-user').value,
 		pass = document.getElementById('input-pass').value;
-	let username = user;
+	username = user;
 	let wif = pass;
 	log ? localStorage.username = username : '';
 	log ? localStorage.wif = JSON.stringify(wif) : {};
@@ -107,16 +139,16 @@ $login.addEventListener('click', () => {
 let $createResourceModalForm = $createResourceModal.querySelector('form');
 $createResourceModalForm.addEventListener('submit', e => {
 	e.preventDefault();
-	let title = this.title.value,
-		combs = this.combs.value,
-		howGet = this.howget.value,
-		description = this.description.value,
-		contacts = this.contacts.value,
+	let title = this['title-resource'].value,
+		combs = this['combs-resource'].value,
+		howGet = this['how-get-resource'].value,
+		description = this['description-resource'].value,
+		contacts = this['contacts-resource'].value,
 		author = username;
 	auth(() => {
 		loadingShow();
 		eos.transaction('beesocial', (operation) => {
-			operation.create(title, 'event', combs + ' HONEYCOMBS', 1, howGet, description, contacts, {authorization: author}); // alice@active
+			operation.resource(author, title, 1, 1, description, howGet, contacts, combs + ' SOCIAL', {authorization: author}); // alice@active
 			loadingHide();
 			$createResourceModalForm.reset();
 			createResourceModal.hide();
